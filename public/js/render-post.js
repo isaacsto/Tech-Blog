0a1f74947814963a217de3bfe90ee987de7d2eb1
Isaac Stofko <isaacstofko@gmail.com>
2023-05-17 17:48:32 -0400

    cleaned up handlebars and frontend js

---
+++ public/js/render-post.js
@@ -0,0 +1,27 @@
+// Function to create new post
+
+const Post = async (event) => {
+    event.preventDefault();
+  
+    // Get form data
+    const title = document.querySelector('#post-title').value.trim();
+    const body = document.querySelector('#post-body').value.trim();
+  
+    // Send POST request to server to create a new post
+    const response = await fetch('/api/blogpost', {
+      method: 'POST',
+      body: JSON.stringify({ title, body }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+  
+    if (response.ok) {
+     
+      document.location.reload();
+    } else {
+      
+      alert('Failed to create a new post');
+    }
+  };
+
+  
+  document.querySelector('.create-post-form').addEventListener('submit', Post);
