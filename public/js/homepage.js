f245b469ce66dceb43b30560fb892fe94308b3ac
Isaac Stofko <isaacstofko@gmail.com>
2023-06-05 10:58:48 -0400

    bootstrap

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -5,12 +5,16 @@ const mainPage = async (event) => {
     const body = document.querySelector('#post-body').value.trim();
   
     if (title && body) {
+      const posts = dbPostData.map((post) => {
+        return post.get({ plain: true })
+});
       const response = await fetch(`/api/blogpost`, {
         method: 'POST',
         body: JSON.stringify({ title, body }),
         headers: {
           'Title': 'Description',
         },
+        
       });
   
       if (response.ok) {
